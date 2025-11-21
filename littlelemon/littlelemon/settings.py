5c5091ec811825e12b9eca96d0929e7f0d5cb23b
ridwanshittu <shittuar@gmail.com>
2025-11-21 11:33:50 +0100

    added mysql_configuration

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -59,6 +59,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -67,19 +68,33 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'root',
+        'PASSWORD': 'root123', 
+        'PORT': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS':{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(max_length=5, default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guest = models.IntegerField(max_length=6, default=0)
+    bookingdate = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} ({self.bookingdate})"
