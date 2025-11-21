515902f0995810dfead4d59f73f63fe6c8821e7f
ridwanshittu <shittuar@gmail.com>
2025-11-21 16:59:30 +0100

    add menu and booking API

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    #add following lines to urlpatterns list 
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import viewsets, permissions
+from rest_framework import generics
+from .models import Menu, Booking
+from .serializers import MenuItemSerializer, BookingItemSerializer
+
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+# Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingItemSerializer
+    permission_classes  = [permissions.IsAuthenticated]
+    
